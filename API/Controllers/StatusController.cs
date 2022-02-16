5a220ce83e5487a2d0bbce51b43117dce50ce23f
Oliver <omoeltoft@gmail.com>
2022-02-16 18:06:15 +0100

    Removed weather & docker

---
+++ API/Controllers/StatusController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        public StatusController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "API is up!";
+        }
+    }
+}
