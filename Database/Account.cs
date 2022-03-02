55a138759c242f20b1c3be5cffad09c639eeb2ec
Oliver <omoeltoft@gmail.com>
2022-03-02 21:25:30 +0100

    EntityFramework med CodeFirst tilføjet

---
+++ Database/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace Database
+{
+    public class Account
+    {
+        public Guid AccountId { get; set; }
+        public int Points { get; set; }
+
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ Database/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Database
+{
+    public class DatabaseContext : DbContext
+    {
+        private static readonly string ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=Minigame;Trusted_Connection=True;";
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
