e04978b8d46aacd1f32a12a908863453e3cd207b
Oliver <omoeltoft@gmail.com>
2022-03-16 17:38:18 +0100

    setup i api til hangmand

---
+++ API/Controllers/HangmanController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using BuisnessLogic.Repository;
+using Hangman;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class HangmanController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly IPrincipal _principal;
+        private readonly IMapper _mapper;
+
+        public HangmanController(IMapper mapper, IAccountRepository accountRepository, IPrincipal principal)
+        {
+            _mapper = mapper;
+            _accountRepository = accountRepository;
+            _principal = principal;
+        }
+
+        [HttpGet]
+        public ActionResult<bool> StartGame()
+        {
+            HangmanGame hangmanGame = new HangmanGame();
+            return true;
+        }
+    }
+}
--- Hangman/HangmanGame.cs
+++ Hangman/HangmanGame.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace Hangman
 {
-    internal class HangmanGame
+    public class HangmanGame
     {
         string word;
         string wrongLetters;
