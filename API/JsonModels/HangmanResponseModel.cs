fffd4aebc6151fbb891b7f5fdf94b98c8e659f86
creoludifico <omoeltoft@gmail.com>
2022-03-23 19:19:40 +0100

    hangman setup

---
+++ API/JsonModels/HangmanResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace API.JsonModels
+{
+    public class HangmanResponseModel
+    {
+        public int Life { get; set; }
+        public bool IsLetterGuessed { get; set; }
+        public bool IsGameWon { get; set; }
+        public bool IsGameLost { get; set; }
+    }
+}
