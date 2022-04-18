1caee54ab2afb0c04c6301904660e6f8c72f6b67
Hildibjørg Didriksen <s164539@student.dtu.dk>
2022-04-18 23:08:06 +0200

    method for creating the startup board

--- TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToeGame.cs
@@ -7,5 +7,26 @@ namespace TicTacToe
     public class Game
     {
 
+        public void createBoard()
+        {
+
+            string[,] board = {
+            {" ", "|", " ", "|", " "},
+            {"-", "+", "-", "+", "-"},
+            {" ", "|", " ", "|", " "},
+            {"-", "+", "-", "+", "-"},
+            {" ", "|", " ", "|", " "}
+            };
+
+            for(int i = 0; i < 5; i++){
+                for (int j = 0; j < 5; j++){
+
+                    Console.Write(board[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+              
     }
 }
