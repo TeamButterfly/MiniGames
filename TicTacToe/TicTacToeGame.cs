2bad6800ea75440c498d94752eeba42c4ae16e96
Hildibjørg Didriksen <s164539@student.dtu.dk>
2022-04-18 23:29:09 +0200

    added move method: prints to the console that an int should be given as input
    added insertMove(): inserts the players' move to the board

--- TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToeGame.cs
@@ -43,6 +43,56 @@ namespace TicTacToe
                 //method
             }
         }
+
+        public void move()
+        {
+            Console.WriteLine("Please enter your placement (1-9):");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x < 0 || x > 9)
+            {
+                Console.WriteLine("Invalid move please try again.");
+                Console.WriteLine("Please enter your placement (1-9):");
+                x = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public void insertMove(int pos, string[,] board)
+        {
+            switch (pos)
+            {
+                case 1:
+                    board[0, 0] = "X";
+                    break;
+                case 2:
+                    board[0, 2] = "X";
+                    break;
+                case 3:
+                    board[0, 4] = "X";
+                    break;
+                case 4:
+                    board[2, 0] = "X";
+                    break;
+                case 5:
+                    board[2, 2] = "X";
+                    break;
+                case 6:
+                    board[2, 4] = "X";
+                    break;
+                case 7:
+                    board[4, 0] = "X";
+                    break;
+                case 8:
+                    board[4, 2] = "X";
+                    break;
+                case 9:
+                    board[4, 4] = "X";
+                    break;
+                
+                default:
+                    break;
+            }
+        }
               
     }
 }
