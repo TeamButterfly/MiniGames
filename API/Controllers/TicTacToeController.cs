03d2692c8440e19860e73c44e3e407864465006d
Hildibjørg <s164539@student.dtu.dk>
2022-04-20 16:12:08 +0200

    Added dependencies and other thing to class

--- API/Controllers/TicTacToeController.cs
+++ API/Controllers/TicTacToeController.cs
@@ -1,7 +1,30 @@
-﻿namespace API.Controllers
+﻿using API.JsonModels;
+using AutoMapper;
+using BuisnessLogic.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+using TicTacToe;
+
+namespace API.Controllers
 {
-    public class TicTacToeController
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class TicTacToeController : ControllerBase
     {
+        private readonly IAccountRepository _accountRepository;
+        private readonly IPrincipal _principal;
+        private readonly IMapper _mapper;
+        private readonly TicTacToeGame _tictactoeGame;
+
+        public TicTacToeController(IMapper mapper, IAccountRepository accountRepository, IPrincipal principal, TicTacToeGame tictactoeGame)
+        {
+            _accountRepository = accountRepository;
+            _mapper = mapper;
+            _principal = principal;
+
+        }
 
     }
 }
